bbbf32751ec1e6442d71cd495ecafdec0b7fccd1
Daniel Vela <daniel@veladan.org>
2025-11-21 22:47:09 +0100

    logger and config

--- Package.swift
+++ Package.swift
@@ -1,42 +1,43 @@
-// swift-tools-version:5.9
+// swift-tools-version:6.1
 import PackageDescription
 
 let package = Package(
-    name: "app-attest-service",
-    platforms: [
-       .macOS(.v10_15)
-    ],
-    dependencies: [
-        // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.110.1"),
-        // 📝 OpenAPI support for Vapor.
-        .package(url: "https://github.com/swift-server/swift-openapi-vapor.git", from: "1.0.1"),
-        // 📝 OpenAPI support for Swift.
-        .package(url: "https://github.com/apple/swift-openapi-generator.git", from: "1.7.0"),
-        // 📝 OpenAPI runtime for Swift.
-        .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.7.0"),
-        // AppAttest support for Swift.
-        .package(url: "https://github.com/iansampson/AppAttest.git", branch: "main"),
-    ],
-    targets: [
-        .executableTarget(
-            name: "App",
-            dependencies: [
-                .product(name: "Vapor", package: "vapor"),
-                .product(name: "OpenAPIVapor", package: "swift-openapi-vapor"),
-                .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
-                .product(name: "AppAttest", package: "AppAttest"),
-            ],
-            plugins: [
-               .plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator"),
-            ]
-        ),
-        .testTarget(
-            name: "AppTests",
-            dependencies: [
-                .target(name: "App"),
-                .product(name: "VaporTesting", package: "vapor"),
-            ]
-        )
-    ]
+  name: "app-attest-service",
+  platforms: [
+    .iOS(.v14),
+    .macOS(.v10_15)
+  ],
+  dependencies: [
+    // 💧 A server-side Swift web framework.
+    .package(url: "https://github.com/vapor/vapor.git", from: "4.110.1"),
+    // 📝 OpenAPI support for Vapor.
+    .package(url: "https://github.com/swift-server/swift-openapi-vapor.git", from: "1.0.1"),
+    // 📝 OpenAPI support for Swift.
+    .package(url: "https://github.com/apple/swift-openapi-generator.git", from: "1.7.0"),
+    // 📝 OpenAPI runtime for Swift.
+    .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.7.0"),
+    // AppAttest support for Swift.
+    .package(url: "https://github.com/iansampson/AppAttest.git", branch: "main"),
+  ],
+  targets: [
+    .executableTarget(
+      name: "App",
+      dependencies: [
+        .product(name: "Vapor", package: "vapor"),
+        .product(name: "OpenAPIVapor", package: "swift-openapi-vapor"),
+        .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
+        .product(name: "AppAttest", package: "AppAttest"),
+      ],
+      plugins: [
+        .plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator"),
+      ]
+    ),
+    .testTarget(
+      name: "AppTests",
+      dependencies: [
+        .target(name: "App"),
+        .product(name: "VaporTesting", package: "vapor"),
+      ]
+    )
+  ]
 )
